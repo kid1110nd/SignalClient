81ab566ca6b94ba9ada3df0b51fb8e53297170a1
luannd <kid1110nd@gmail.com>
2019-12-16 17:10:56 +0700

    Up code

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+interface IDisplayMedia {
+    getDisplayMedia(constraints: MediaStreamConstraints): Promise<MediaStream>;
+  }
+  interface MediaDevices extends IDisplayMedia {
+  }
+  
+  interface Navigator extends IDisplayMedia {
+  }
+  
